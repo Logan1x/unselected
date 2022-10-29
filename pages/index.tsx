14bbf4b6f414bc75b26faac15cfcaa8067bd851a
logan1x <sharmakhushal78@gmail.com>
2022-10-29 18:52:23 +0530

    imprv : styled hero icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,36 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-semibold">
+        <h1 className="text-6xl font-medium text-blue-600 flex items-center">
           <span className="font-light line-through text-gray-500">un</span>
-          <a className="text-blue-600" href="https://nextjs.org">
-            selected
-          </a>
+          <span className="bg-gray-200 p-1 rounded">selected</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-12 h-12 text-blue-700"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-12 h-12 text-blue-700"
+          >
+            <path
+              fillRule="evenodd"
+              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+              clipRule="evenodd"
+            />
+          </svg>
         </h1>
 
         <p className="mt-3 text-2xl text-gray-700">
