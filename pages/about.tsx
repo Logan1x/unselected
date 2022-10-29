47662af351d87aeac3f31ad085dbce272530a2b7
logan1x <sharmakhushal78@gmail.com>
2022-10-29 17:35:11 +0530

    Feat: about page, route, footer

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,6 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 
-const Home: NextPage = () => {
+export default function about() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -10,22 +9,19 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-semibold">
-          <span className="font-light line-through text-gray-500">un</span>
-          <a className="text-blue-600" href="https://nextjs.org">
-            selected
-          </a>
-        </h1>
+        <section>
+          <h1 className="text-3xl text-gray-500">about</h1>
 
-        <p className="mt-3 text-2xl text-gray-700">
-          Let AI write your cover letter{" "}
-        </p>
+          <p className="mt-3 text-xl text-gray-700">
+            Let AI write your cover letter{" "}
+          </p>
+        </section>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-around border-t">
         <p>
           <span className="border-b-2 border-gray-700 hover:border-blue-600 hover:border-b-4">
-            <a href="/about">about</a>
+            <a href="/">home</a>
           </span>
         </p>
         <p>
@@ -41,6 +37,4 @@ const Home: NextPage = () => {
       </footer>
     </div>
   );
-};
-
-export default Home;
+}
